5e7cfdc5cf9ab387919f3d8e565511bb32d1a9c3
XFerTz <2402999@utrivieramaya.edu.mx>
2024-10-23 07:50:00 -0500

    first commit

---
+++ js/typeof.js
@@ -0,0 +1,7 @@
+console.log(typeof 'Fer') //string
+console.log(typeof 25)     /*number*/
+console.log(typeof true)   //boolean
+console.log(typeof null)    //object type
+console.log(typeof undefined) //undefined
+
+// los comentarios pueden hacer que los codigos sean legibles
---
+++ js/variable.js
@@ -0,0 +1,33 @@
+//declare variables y asigne tipos de datos de cadena, booleanos. indefinidos y nulos.
+
+let nombre = 'Fer'; //string 
+let status = false; //boolean
+let city; //indefinido
+let address = null; //null
+
+let materia1;
+let materia2;
+let materia3;
+let manteria4;
+
+let profesor = 'Pino';
+let profesor1 = 'Perla';
+let profesor2 = 'Geovani';
+let profesor3 = 'Albinito';
+
+let myName = 'Fer';
+let myLastName = 'Caamal'
+let isMarried = false;
+let country = 'mexico';
+let age = '20'
+
+let myName1, myLastName1, isMarried1, country1, age1;
+
+Name1 = 'nose';
+Name1 = 'nose2';
+Name1 = 'nose3';
+
+let myAge = '21';
+let yourAge = '25';
+
+console.log('Mi edad es' + ' ' + myAge, 'Tu edad es ' + yourAge)
